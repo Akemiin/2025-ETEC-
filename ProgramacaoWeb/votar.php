d1ef481440a71e1a2facce765b350d91a14fd64f
Akemi <146261409+Akemiin@users.noreply.github.com>
2025-03-26 13:35:30 -0300

    Add files via upload

---
+++ ProgramacaoWeb/votar.php
@@ -0,0 +1,25 @@
+<h1>Aula 05 - Funções</h1>
+
+<?php
+
+function votar(){
+    $idade = 16;
+
+    if( $idade >= 18){
+        $resultado = "Você deve votar!";
+    }
+
+    else if( $idade >= 16 && $idade <= 17){
+        $resultado = "Você já pode votar!";
+    }
+
+    else{
+        $resultado = "Você ainda é muito novo para votar";
+    }
+
+    echo $resultado;
+}
+
+    votar();
+
+?>
